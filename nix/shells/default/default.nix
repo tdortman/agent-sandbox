eb184248b74d8487cd7830cfc9ab229d0c0a539d
Tim Dortmann <git@tdortman.com>
2026-06-25 04:23:14 +0200

    build(nix): add QtGui headers to dev shell
    Expose QtGui include paths needed to build qt-helper locally.

--- nix/shells/default/default.nix
+++ nix/shells/default/default.nix
@@ -24,6 +24,8 @@ pkgs.mkShell {
       + ":"
       + "${pkgs.qt6.qtbase.out}/include/QtWidgets"
       + ":"
+      + "${pkgs.qt6.qtbase.out}/include/QtGui"
+      + ":"
       + "${pkgs.nodejs}/include/node";
   };
 }
